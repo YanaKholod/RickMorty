3bc667cdeb1f2be01d116f79405fc550f2f991fe
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-10-31 18:02:09 +0200

    pagination created

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -30,17 +30,58 @@ const FilterInput = styled.input`
   border-radius: 4px;
   margin-right: 10px;
 `;
-function CharacterList() {
+const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  button {
+    font-weight: bold;
+    font-size: 16px;
+    margin: 0 10px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    &:hover {
+    }
+  }
+  span {
+    font-weight: bold;
+  }
+  select {
+    border-radius: 4px;
+    padding: 5px;
+    background-color: transparent;
+    color: white;
+  }
+`;
+const CharacterList = () => {
   const dispatch = useDispatch();
   const [nameFilter, setNameFilter] = useState("");
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const [totalPages, setTotalPages] = useState(42);
   const charactersArray = useSelector(
     (state) => state.characters.charactersArray
   );
   const error = useSelector((state) => state.characters.error);
 
   useEffect(() => {
-    dispatch(fetchAllCharacters());
-  }, [dispatch]);
+    dispatch(fetchAllCharacters({ page }));
+  }, [dispatch, page, perPage]);
+
+  const handlePageChange = async (newPage) => {
+    if (page <= totalPages) {
+      await setPage(newPage);
+    }
+  };
+
+  const handlePerPageChange = (newPerPage) => {
+    setPerPage(newPerPage);
+    setPage(1);
+  };
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -49,7 +90,7 @@ function CharacterList() {
   const filteredCharacters = charactersArray.filter((character) =>
     character.name.toLowerCase().includes(nameFilter.toLowerCase())
   );
-
+  console.log("CHCHCHCH", charactersArray);
   return (
     <div>
       <FilterContainer>
@@ -65,8 +106,23 @@ function CharacterList() {
           <CharacterItem key={character.id} character={character} />
         ))}
       </CharacterListContainer>
+      <Pagination>
+        <button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+        >
+          Previous
+        </button>
+        <span> Page {page} </span>
+        <button
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === totalPages}
+        >
+          Next
+        </button>
+      </Pagination>
     </div>
   );
-}
+};
 
 export default CharacterList;
