0c1354e5b7b15291a8e4c5f9342f966ae4517d28
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-10-31 17:31:30 +0200

    upd

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,13 +1,5 @@
 import CharacterDetailsPage from "../components/CharacterDetailsPage";
-import styled from "styled-components";
 
-const Styled = {
-  Wrapper: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `,
-};
 const Details = () => {
   return <CharacterDetailsPage />;
 };
